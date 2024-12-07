c4c7b0b2e1a9d1c40e54e06cb660f2e9fcb78f36
REMR11 <remr11@ufg.edu.sv>
2024-12-07 17:33:40 -0600

    Feat: Crea query methods

--- src/main/java/com/Kodigo/pizzeria/Repository/PizzaRepository.java
+++ src/main/java/com/Kodigo/pizzeria/Repository/PizzaRepository.java
@@ -4,6 +4,19 @@ import com.Kodigo.pizzeria.Models.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+    List<Pizza> findByNombreContainingIgnoreCase(String nombre);
+
+    List<Pizza> findByVegetarianaTrue();
+
+    List<Pizza> findByPrecioGreaterThan(double precioMinimo);
+
+    List<Pizza> findByDescripcionContainingIgnoreCase(String ingrediente);
+
+    List<Pizza> findByPrecioBetween(double precioMinimo, double precioMaximo);
+
+    List<Pizza> findByDisponibleTrueAndVegetarianaTrueOrVeganaTrue();
 }
